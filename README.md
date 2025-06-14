354470b0e248a718407622cde85a1d429d680470
Psionik K <73710933+psionic-k@users.noreply.github.com>
2025-06-14 20:01:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Pins
+
+This repo demonstrates a solution for centralizing all of your dependency
+declarations for your projects.
+
+The contained Nix flake provides the inputs that our projects use.  Because of
+how Nix lockfiles work, udpates to dependent projects are lazy and you can
+update them individually whenever it is convenient.  Projects can also override
+the flake in-detail.  It provides the advantages of central declaration while
+retaining the flexibility of polyrepo.
+
+It is made available by adding this flake to your registry:
+
+```
+nix registry add pins git+https://github.com/positron-solutions/pins.git
+```
+
+In downstream projects, we declare our inputs relative to pins, like so:
+
+```nix
+  inputs = {
+    pins.url = "pins";
+
+    systems.follows = "pins/systems";
+    nixpkgs.follows = "pins/nixpkgs";
+    fenix.follows = "pins/fenix";
+    crane.follows = "pins/crane";
+    flake-parts.follows = "pins/flake-parts";
+  };
+```
+
+
+To reveal that all inputs share dependency versions, show the dependency tree:
+```
+nix flake metadata
+```
+
+## Maintenance
+
+Every six months or so, Nixpkgs cuts a new branch.  Update the `24.11` names in
+the flake.nix and then update the lockfile:
+
+```
+nix flake lock --update-input nixpkgs
+```
+
+Update other inputs using similar commands as desired.  Do not update them too
+frequently without updating pins in downstream projects:
+
+```
+nix flake lock --update-input pins
+```
+
+## Pinned Flakes
+
+The Fenix flake should be of particular interest.  It effectively pins a Rust
+version.  Updating Fenix will therefore update the version of Rust that is
+avaialable in downstream.  Again, just update the input for Fenix in each
+project.  If a project doesn't work, roll back its lock file and fix it when you
+get more time.
+
+Having nixpkgs pinned can also speed up a lot of operations.  In the Nix
+registry, you can make your nixpkgs depend on the version found in this repo.
+Instead of downloading new versions of nixpkgs for basic commands like `nix
+search`, the same version of packages will be used.  If you add a package to a
+project, it will be the same version as one obtained from running `nix shell`
---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Centrally pinned inputs for our projects.";
+  inputs  = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
+    crane.url = "github:ipetkov/crane";
+
+    # TODO vendor this.  Upstream is tracking nixpkgs master.  This leads to
+    # dispersion between our nixpkgs and the lib version used by flake-parts.
+    # At least it is pinned.
+    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+      url = "github:hercules-ci/flake-parts";
+    };
+  };
+
+  # Most of this flake is used by referencing the inputs above rather than
+  # declaring any real outputs.  However, the outputs can be used, as below, to
+  # expose things like overlays for downstream projects.
+  outputs = inputs: {
+    # You can use overlays within pinning to gather up overrides of packages.
+    # Just pass in the overlays when instantiating the nixpkgs for a project and
+    # it will have the packages available.
+    overlays.default = final: prev: {
+      # cargo leptos versions and wasm bindgen versions must upgrade
+      # together.  This override can be used in individual project shells to
+      # temporarilly decouple that project
+      cargo-leptos-2-28 = prev.rustPlatform.buildRustPackage rec {
+        pname = "cargo-leptos";
+        version = "0.2.28";
+
+        src = prev.fetchFromGitHub {
+          owner = "leptos-rs";
+          repo = "cargo-leptos";
+          rev = "v${version}";
+          hash = "sha256-SjpfM963Zux+H5QhK8prvDLuI56fP5PqX5gcVbthRx4=";
+        };
+
+        cargoHash = if prev.stdenv.isDarwin
+        then "sha256-Da9ei4yAOfhSQmQgrUDZCmMeJXTfGnYhI1+L0JT/ECs="
+        else "sha256-7nFDftc468mB3TbnKbEb5xEYEQzTtgFaX6uWodmLwRI=";
+
+        buildInputs = prev.lib.optionals prev.stdenv.isDarwin [
+          prev.darwin.apple_sdk.frameworks.SystemConfiguration
+          prev.darwin.apple_sdk.frameworks.Security
+          prev.darwin.apple_sdk.frameworks.CoreServices
+        ];
+
+        buildFeatures = [ "no_downloads" ];
+        doCheck = false;
+
+        meta = with prev.lib; {
+          description = "Build tool for the Leptos web framework";
+          mainProgram = "cargo-leptos";
+          homepage = "https://github.com/leptos-rs/cargo-leptos";
+          license = [ licenses.mit ];
+        };
+      };
+    };
+  };
+}
